54d6d6c9a49f666937d8fef35545f408ed38e1cd
Mary-Anne Triggs <maryannetriggs@gmail.com>
2020-02-14 16:06:59 +0000

    bobs budget project complete

--- codecademy-php/bobs-budget/index.php
+++ codecademy-php/bobs-budget/index.php
@@ -16,11 +16,36 @@ $socialSecurity = $grossSalary * .062;
 
 $incomeSegments = [[9700, .88], [29775, .78], [8675, .76]];
 
-// Write your code below:
-
 $netIncome = (($incomeSegments[0][0] * $incomeSegments[0][1]) + ($incomeSegments[1][0] * $incomeSegments[1][1]) + ($incomeSegments[2][0] * $incomeSegments[2][1]));
 
 $netIncome -= $socialSecurity;
 $annualIncome = $netIncome;
 
 echo "Annual income before deducting annual expenses: $$annualIncome\n";
+
+$monthlyIncome = $annualIncome / 12;
+echo "Monthly income before deducting annual expenses: $$monthlyIncome\n";
+
+$monthlyIncome -= $monthlyExpenses["rent"];
+$monthlyIncome -= $monthlyExpenses["utilities"];
+$monthlyIncome -= $monthlyExpenses["healthInsurance"];
+
+echo "Monthly income after deducting annual expenses: $$monthlyIncome\n";
+
+$weeklyIncome = $monthlyIncome / 4.33;
+echo "Weekly income before deducting weekly expenses: $$weeklyIncome\n";
+
+$weeklyExpenses = [
+  "gas" => 25,
+  "food" => 90,
+  "entertainment" => 47
+];
+
+$weeklyIncome -= $weeklyExpenses["gas"];
+$weeklyIncome -= $weeklyExpenses["food"];
+$weeklyIncome -= $weeklyExpenses["entertainment"];
+
+echo "Weekly income after deducting week expenses: $$weeklyIncome\n";
+
+$annualSavings = $weeklyIncome * 52;
+echo "Annual savings possible over the year: $" . round($annualSavings);
