1be7458d341e674eae344da620e47d4efc71dabf
Mary-Anne Triggs <maryannetriggs@gmail.com>
2020-02-14 15:55:07 +0000

    bobs budget project begun

---
+++ codecademy-php/bobs-budget/index.php
@@ -0,0 +1,26 @@
+<?php
+  
+$annualExpenses = [
+    "vacations" => 1000,
+    "carRepairs" => 1000,    
+];
+
+$monthlyExpenses = [
+    "rent" => 1500,
+    "utilities" => 200,
+    "healthInsurance" => 200
+];
+
+$grossSalary = 48150;
+$socialSecurity = $grossSalary * .062;
+
+$incomeSegments = [[9700, .88], [29775, .78], [8675, .76]];
+
+// Write your code below:
+
+$netIncome = (($incomeSegments[0][0] * $incomeSegments[0][1]) + ($incomeSegments[1][0] * $incomeSegments[1][1]) + ($incomeSegments[2][0] * $incomeSegments[2][1]));
+
+$netIncome -= $socialSecurity;
+$annualIncome = $netIncome;
+
+echo "Annual income before deducting annual expenses: $$annualIncome\n";
