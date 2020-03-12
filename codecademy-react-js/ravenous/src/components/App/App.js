f6993b0b1c06b311545d4519d933af528f009c3b
Mary-Anne Triggs <maryannetriggs@gmail.com>
2020-03-12 20:27:29 +0000

    part 1 ravenous project complete

---
+++ codecademy-react-js/ravenous/src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
+
+import './App.css'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar/>
+        <BusinessList/> 
+      </div>
+    )
+  }
+  
+}
+
+export default App
